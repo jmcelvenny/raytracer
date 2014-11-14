4bf39d6b42f56c3cecd74182817d74fcdccf4475
jmcelvenny <siriusx96@yahoo.com>
2014-11-14 11:57:21 -0500

    Added Super Sampling

--- demo/example.cpp
+++ demo/example.cpp
@@ -8,9 +8,11 @@
 #include <vector>
 #include <memory>
 #include <random>
-#include <omp.h>
 
-// Uncomment when you have the sphere implemented
+#ifdef _OPENMP
+  #include <omp.h>
+#endif
+
 #include "Sphere.h"
 #include "LambertianShader.h"
 #include "DirectionLight.h"
@@ -93,9 +95,9 @@ int main(int argc, char* argv[]) {
     // Example of adding sphere to a vector of Shape*.  Can use to render multiple sphere
     // Based on depth
     Point p(0,10,0);
-    std::shared_ptr<PointLight> light2 (new PointLight(Color(.6275, 1, .6667, 1), 1.f, p));
-    //std::shared_ptr<DirectionLight> light3 (new DirectionLight(Color(.6275,1,.6667,1), 1.f, Vector(-4,-3,0)));
-    //std::shared_ptr<DirectionLight> light (new DirectionLight(Color(.6275, 1, .6667,1), 1.f, Vector(4, 3, 0)));
+    std::shared_ptr<PointLight> light (new PointLight(Color(.6275, 1, .6667, 1), 1.f, p));
+    //std::shared_ptr<DirectionLight> light2 (new DirectionLight(Color(.6275,1,.6667,1), 1.f, Vector(-4,-3,0)));
+    //std::shared_ptr<DirectionLight> light3 (new DirectionLight(Color(.6275, 1, .6667,1), 1.f, Vector(4, 3, 0)));
     std::shared_ptr<Shader> shader(new LambertianShader(Color(.6275,1,.666667,1)));
     std::shared_ptr<Sphere> sphere1 (new Sphere(transform1, 5.0f, -10.0f, 10.0f, 360.0f));
     std::shared_ptr<Sphere> sphere2 (new Sphere(transform2, 5.0f, -10.0f, 10.0f, 360.0f));
@@ -104,58 +106,46 @@ int main(int argc, char* argv[]) {
     std::shared_ptr<Primitive> s2 (new Primitive(sphere2, shader));
 
     Scene &scene = Scene::getInstance();
-    //scene.addLight(light);
-    scene.addLight(light2);
-    //scene.addLight(light3);
+    scene.addLight(light);
     scene.addPrimitive(s1);
     scene.addPrimitive(s2);
 
 
-//    Sphere *sphere2 = new Sphere(transform2, 10.0f);
-
+    int samples=cmd.find("-samples", 4);
     /***********************Do Raycasting Here******************************/
     #pragma omp parallel for
     for (int r = 0; r < image.getHeight(); ++r) {
       for (int c = 0; c < image.getWidth(); ++c) {
-        // Create a color
-        Color color;
-
-        // Uncomment when you have the sphere implemented
-
-
-        // Used to determine which shape is in front
-        float closest_thit = std::numeric_limits<float>::infinity();
-
-	//SUPER DUPER SAMPLING
-	// */
-        Ray ray(camera.eye(), camera.view((float)(c/*+variance_x*/)/image.getWidth(), (float)(r/*+variance_y*/)/image.getHeight()));
+        Color colors[samples];
+        for (int s=0; s<samples; s++) {
+          // Create a color
+          Color color;
+          float closest_thit = std::numeric_limits<float>::infinity();
 
 
-  	// You can use a for loop to determine which shape is the closest with a hit test
+          Ray ray(camera.eye(), camera.view((float)(c)/image.getWidth(), (float)(r)/image.getHeight()));
+
           float thit = std::numeric_limits<float>::infinity();
           std::shared_ptr<DifferentialGeometry> dg;
   	      std::shared_ptr<Primitive> primitive;
-  	  // If you hit the shape, compare its t value (thit) with the previously recorded one
-  	  // If it is the closest, record the index value
+
           if(scene.hit(ray, thit, dg, primitive)) {
             if (thit < closest_thit) {
               closest_thit = thit;
   	          color = primitive->shade(dg);
             }
           }
-
-    /*    //calculate the average color
-        int ar=0, ag=0, ab=0;
+          colors[s]=color;
+        }
+        //calculate the average color
+        float ar=0, ag=0, ab=0;
         for (int i = 0; i<samples; ++i) {
           ar+=colors[i].red;
           ag+=colors[i].green;
           ab+=colors[i].blue;
         }
-        float cr = ar/samples;
-        float cg = ag/samples;
-        float cb = ab/samples;
-        image.setPixel(r, c, Color(cr, cg, cb, 1)); */
-	image.setPixel(r,c,color);
+        Color color((float)ar/samples, (float)ag/samples, (float)ab/samples, 1);
+	      image.setPixel(r,c,color);
       }
     }
 
